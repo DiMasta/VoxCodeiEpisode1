485f39e5e6e939a01053998e91b08a33d570fab7
Vladislav Vulchev <vvladoh@gmail.com>
2019-04-12 20:46:09 +0300

    Counting bombs in range

--- VoxCodeiEpisode1/voxCodeiEpisode1.cpp
+++ VoxCodeiEpisode1/voxCodeiEpisode1.cpp
@@ -25,16 +25,38 @@ using namespace std;
 const string INPUT_FILE_NAME = "input.txt";
 const string OUTPUT_FILE_NAME = "output.txt";
 
-const int INVALID_ID = -1;
-const int INVALID_NODE_DEPTH = -1;
-const int TREE_ROOT_NODE_DEPTH = 1;
-const int ZERO_CHAR = '0';
-const int DIRECTIONS_COUNT = 8;
-const int BYTE_SIZE = 8;
-const int PAIR = 2;
+static const int INVALID_ID = -1;
+static const int INVALID_NODE_DEPTH = -1;
+static const int TREE_ROOT_NODE_DEPTH = 1;
+static const int ZERO_CHAR = '0';
+static const int DIRECTIONS_COUNT = 4;  // UP, DOWN, LEFT, RIGHT
+static const int BYTE_SIZE = 8;
+static const int PAIR = 2;
 
-const int MAX_HEIGHT = 19;
-const int MAX_WIDTH = 19;
+static const int MAX_HEIGHT = 19;
+static const int MAX_WIDTH = 19;
+static const int BOMB_RADIUS = 3;
+
+static const char EMPTY = '.';
+static const char WALL = '#';
+static const char SURVEILLANCE_NODE = '@';
+
+enum class Direction : int {
+	UP = 0,
+	DOWN = 1,
+	LEFT = 2,
+	RIGHT = 3,
+};
+
+static const Direction directions[] = {
+	Direction::UP,
+	Direction::DOWN,
+	Direction::LEFT,
+	Direction::RIGHT
+};
+
+static const int MOVE_IN_ROWS[DIRECTIONS_COUNT] = { 1, -1, 0, 0 };
+static const int MOVE_IN_COLS[DIRECTIONS_COUNT] = { 0, 0, -1, 1 };
 
 //-------------------------------------------------------------------------------------------------------------
 //-------------------------------------------------------------------------------------------------------------
@@ -54,7 +76,36 @@ public:
 		return width;
 	}
 
+	/// Set cell entry, without checking for valid coordinates, which may be a mistake
+	/// @param[in] rowIdx the index of the row, where the cell will be set
+	/// @param[in] colIdx the index of the column, where the cell will be set
+	/// @param[in] cell the value for the cell, that will be set
 	void setCell(int rowIdx, int colIdx, char cell);
+
+	/// Get cell, without checking for valid coordinates, which may be a mistake
+	/// @param[in] rowIdx the index of the row, for the cell
+	/// @param[in] colIdx the index of the column, for the cell
+	/// @return the cell value
+	char getCell(int rowIdx, int colIdx) const;
+
+	/// Iterate the whole grid and set for each cell how many surveillance nodes will be destroyed if a bomb is set there
+	/// This hides the risk when a bomb is detonated it changes the cell evaluation, but I think for starting marking
+	/// of the best cells to check is OK
+	void evaluateGridCells();
+
+	/// Count how many surveillance nodes will be affected if a bomb is placed in the cell with the given coordinates
+	/// @param[in] rowIdx the index of the row, for the cell to check
+	/// @param[in] colIdx the index of the column, for the cell to check
+	/// @return surveillance nodes in range count
+	int countSurveillanceNodesInRange(int rowIdx, int colIdx) const;
+
+	/// Count how many surveillance nodes will be affected, in certain direction,
+	/// if a bomb is placed in the cell with the given coordinates
+	/// @param[in] rowIdx the index of the row, for the cell to check
+	/// @param[in] colIdx the index of the column, for the cell to check
+	/// @param[in] direction the direction for which to check
+	/// @return surveillance nodes in range count
+	int countSurveillanceNodesInRangeForDirection(int rowIdx, int colIdx, Direction direction) const;
 
 private:
 	char grid[MAX_HEIGHT][MAX_WIDTH];
@@ -68,6 +119,72 @@ private:
 
 void Grid::setCell(int rowIdx, int colIdx, char cell) {
 	grid[rowIdx][colIdx] = cell;
+}
+
+//*************************************************************************************************************
+//*************************************************************************************************************
+
+char Grid::getCell(int rowIdx, int colIdx) const {
+	return grid[rowIdx][colIdx];
+}
+
+//*************************************************************************************************************
+//*************************************************************************************************************
+
+void Grid::evaluateGridCells() {
+	for (int rowIdx = 0; rowIdx < height; ++rowIdx) {
+		for (int colIdx = 0; colIdx < width; ++colIdx) {
+			char& cell = grid[rowIdx][colIdx];
+
+			if (EMPTY == cell) {
+				cell = static_cast<int>(countSurveillanceNodesInRange(rowIdx, colIdx));
+			}
+		}
+	}
+}
+
+//*************************************************************************************************************
+//*************************************************************************************************************
+
+int Grid::countSurveillanceNodesInRange(int rowIdx, int colIdx) const {
+	int affectedNodesCount = 0;
+
+	for (const Direction direction : directions) {
+		affectedNodesCount += countSurveillanceNodesInRangeForDirection(rowIdx, colIdx, direction);
+	}
+
+	return affectedNodesCount;
+}
+
+//*************************************************************************************************************
+//*************************************************************************************************************
+
+int Grid::countSurveillanceNodesInRangeForDirection(int rowIdx, int colIdx, Direction direction) const {
+	int affectedNodesCount = 0;
+
+	for (int range = 0; range < BOMB_RADIUS; ++range) {
+		rowIdx += MOVE_IN_ROWS[static_cast<int>(direction)];
+		colIdx += MOVE_IN_COLS[static_cast<int>(direction)];
+
+		if (rowIdx >= 0 && rowIdx < height && colIdx >= 0 && colIdx < width) {
+			const char cell = getCell(rowIdx, colIdx);
+
+			if (WALL == cell) {
+				break;
+			}
+
+			if (SURVEILLANCE_NODE == cell) {
+				++affectedNodesCount;
+			}
+
+			// Do not considering other bombs or empty cells
+		}
+		else {
+			break;
+		}
+	}
+
+	return affectedNodesCount;
 }
 
 //-------------------------------------------------------------------------------------------------------------
@@ -124,6 +241,7 @@ void Game::initGame() {
 //*************************************************************************************************************
 
 void Game::gameBegin() {
+	firewallGrid.evaluateGridCells();
 }
 
 //*************************************************************************************************************
